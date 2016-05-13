ae046e0097de804139e954868c309da2b4088b43
kanglg <kanglgt@163.com>
2016-05-13 17:43:07 +0800

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularAppApp
+ * @description
+ * # angularAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularAppApp', [
+    'ngResource'
+  ]);

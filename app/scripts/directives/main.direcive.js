7f2f86917f68155a39b500c89a734c2b6ccd0f43
kanglg <kanglgt@163.com>
2016-05-17 17:07:00 +0800

    build starter

--- app/scripts/directives/main.direcive.js
+++ app/scripts/directives/main.direcive.js
@@ -2,7 +2,7 @@
  * Created by kanglg on 16/5/16.
  */
 'use strict';
-(function() {
+(function () {
   var options = {
     //Add slimscroll to navbar menus
     //This requires you to load the slimscroll plugin
@@ -100,23 +100,23 @@
       lg: 1200
     }
   };
-  var lteLayoutDirective = ['$window', function($window) {
+  var lteLayoutDirective = [function () {
+    var $ = angular.element;
     return {
       restrict: 'A',
-      require: '?^lte',
-      link: function(scope, element, attr, ctrl) {
-        var options = ctrl.$options;
-        var layout = {
-          activate: function () {
+      require: ['?^lte', 'lteLayout'],
+      controller: [function () {
+        this.$layout = {
+          activate: function (options) {
             var _this = this;
-            _this.fix();
-            _this.fixSidebar();
+            _this.fix(options);
+            _this.fixSidebar(options);
             $(window, ".wrapper").resize(function () {
-              _this.fix();
-              _this.fixSidebar();
+              _this.fix(options);
+              _this.fixSidebar(options);
             });
           },
-          fix: function () {
+          fix: function (options) {
             //Get window height and the wrapper height
             var neg = $('.main-header').outerHeight() + $('.main-footer').outerHeight();
             var window_height = $(window).height();
@@ -138,19 +138,20 @@
               //Fix for the control sidebar height
               var controlSidebar = $(options.controlSidebarOptions.selector);
               if (typeof controlSidebar !== "undefined") {
-                if (controlSidebar.height() > postSetWidth)
+                if (controlSidebar.height() > postSetWidth) {
                   $(".content-wrapper, .right-side").css('min-height', controlSidebar.height());
+                }
               }
             }
           },
-          fixSidebar: function () {
+          fixSidebar: function (options) {
             //Make sure the body tag has the .fixed clclassass
             if (!$("body").hasClass("fixed")) {
-              if (typeof $.fn.slimScroll != 'undefined') {
+              if (typeof $.fn.slimScroll !== 'undefined') {
                 $(".sidebar").slimScroll({destroy: true}).height("auto");
               }
               return;
-            } else if (typeof $.fn.slimScroll == 'undefined' && window.console) {
+            } else if (typeof $.fn.slimScroll === 'undefined' && window.console) {
               window.console.error("Error: the fixed layout requires the slimscroll plugin!");
             }
             //Enable slimscroll for fixed layout
@@ -168,22 +169,157 @@
             }
           }
         };
+      }],
+      link: function (scope, element, attr, ctrls) {
+        var options = ctrls[0].$options;
+        var layout = ctrls[1].$layout;
+        //Fix for IE page transitions
+        $("body").removeClass("hold-transition");
         layout.activate(options);
+        scope.$watch(options.sidebarSlimScroll, function () {
+          layout.fix(options);
+          layout.fixSidebar(options);
+        });
       }
-    }
+    };
   }];
-  var lteController = ['$scope', '$attrs', '_', function($scope, $attrs, _) {
+  var lteController = ['$scope', '$attrs', '_', function ($scope, $attrs, _) {
     var $options = $scope.$eval($attrs.lte) || {};
     this.$options = _.extend({}, options, $options);
   }];
-  var lteDirective = [function() {
+  var lteDirective = [function () {
     return {
       restrict: 'A',
       controller: lteController
     }
   }];
+  var lteSidebarDirective = [function () {
+    return {
+      restrict: 'A',
+      require: ['?^lte', '?^lteLayout'],
+      link: function (scope, element, attrs, ctrls) {
+        var $options = ctrls[0].$options;
+        var $layout = ctrls[1].$layout;
+        var animationSpeed = $options.animationSpeed;
+        element.on('click', 'li a', function (e) {
+          var $this = $(this);
+          var checkElement = $this.next();
+          //Check if the next element is a menu and is visible
+          if ((checkElement.is('.treeview-menu')) && (checkElement.is(':visible'))) {
+            //Close the menu
+            checkElement.slideUp(animationSpeed, function () {
+              checkElement.removeClass('menu-open');
+              //Fix the layout in case the sidebar stretches over the height of the window
+              //_this.layout.fix();
+            });
+            checkElement.parent("li").removeClass("active");
+          }//If the menu is not visible
+          else if ((checkElement.is('.treeview-menu')) && (!checkElement.is(':visible'))) {
+            //Get the parent menu
+            var parent = $this.parents('ul').first();
+            //Close all open menus within the parent
+            var ul = parent.find('ul:visible').slideUp(animationSpeed);
+            //Remove the menu-open class from the parent
+            ul.removeClass('menu-open');
+            //Get the parent li
+            var parent_li = $this.parent("li");
+
+            //Open the target menu and add the menu-open class
+            checkElement.slideDown(animationSpeed, function () {
+              //Add the class active to the parent li
+              checkElement.addClass('menu-open');
+              parent.find('li.active').removeClass('active');
+              parent_li.addClass('active');
+              //Fix the layout in case the sidebar stretches over the height of the window
+              $layout.fix($options);
+            });
+          }
+          //if this isn't a link, prevent the page from being redirected
+          if (checkElement.is('.treeview-menu')) {
+            e.preventDefault();
+          }
+        });
+      }
+    }
+  }];
+
+  var ltePushmenuDirective = ['$window', function ($window) {
+    var $ = angular.element;
+    return {
+      restrict: 'A',
+      require: '?^lte',
+      link: function (scope, element, attrs, ctrl) {
+        var $options = ctrl.$options;
+        //Get the screen sizes
+        var screenSizes = $options.screenSizes;
+        //Enable sidebar toggle
+        element.on('click', function (e) {
+          e.preventDefault();
+          //Enable sidebar push menu
+          if ($($window).width() > (screenSizes.sm - 1)) {
+            if ($("body").hasClass('sidebar-collapse')) {
+              $("body").removeClass('sidebar-collapse').trigger('expanded.pushMenu');
+            } else {
+              $("body").addClass('sidebar-collapse').trigger('collapsed.pushMenu');
+            }
+          }
+          //Handle sidebar push menu for small screens
+          else {
+            if ($("body").hasClass('sidebar-open')) {
+              $("body").removeClass('sidebar-open').removeClass('sidebar-collapse').trigger('collapsed.pushMenu');
+            } else {
+              $("body").addClass('sidebar-open').trigger('expanded.pushMenu');
+            }
+          }
+        });
+        $(".content-wrapper").click(function () {
+          //Enable hide menu when clicking on the content-wrapper on small screens
+          if ($($window).width() <= (screenSizes.sm - 1) && $("body").hasClass("sidebar-open")) {
+            $("body").removeClass('sidebar-open');
+          }
+        });
+
+        var expand = function () {
+          $("body").removeClass('sidebar-collapse').addClass('sidebar-expanded-on-hover');
+        };
+
+        var collapse = function () {
+          if ($('body').hasClass('sidebar-expanded-on-hover')) {
+            $('body').removeClass('sidebar-expanded-on-hover').addClass('sidebar-collapse');
+          }
+        };
+
+        var expandOnHover = function() {
+          var screenWidth = $options.screenSizes.sm - 1;
+          //Expand sidebar on hover
+          $('.main-sidebar').hover(function () {
+            if ($('body').hasClass('sidebar-mini')
+              && $("body").hasClass('sidebar-collapse')
+              && $window.width() > screenWidth) {
+              expand();
+            }
+          }, function () {
+            if ($('body').hasClass('sidebar-mini')
+              && $('body').hasClass('sidebar-expanded-on-hover')
+              && $window.width() > screenWidth) {
+              collapse();
+            }
+          });
+        };
+
+        //Enable expand on hover for sidebar mini
+        if ($options.sidebarExpandOnHover
+          || ($('body').hasClass('fixed')
+          && $('body').hasClass('sidebar-mini'))) {
+          expandOnHover();
+        }
+      }
+    }
+  }];
 
   angular.module('angularApp')
     .directive('lteLayout', lteLayoutDirective)
+    .directive('lteSidebar', lteSidebarDirective)
+    .directive('ltePushmenu', ltePushmenuDirective)
     .directive('lte', lteDirective);
 })();
--- app/scripts/services/underscore.js
+++ app/scripts/services/underscore.js
@@ -1,6 +1,7 @@
 /**
  * Created by kanglg on 16/5/10.
  */
+'use strict';
 angular.module('underscore', []).factory('_', ['$window', function($window) {
     return $window._;
 }]);
